e6b85114acd644af8cf15e715fa6ee31eeafd771
Pranav <pranavbmcloud@gmail.com>
2021-01-02 14:33:55 +0530

    adds test_local_ingest_should_return_file_data

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -10,3 +10,8 @@ class TestLocalIngest(unittest.TestCase):
     def test_local_ingest_without_filepath_raises_TypeError(self):
         with self.assertRaises(TypeError):
             local_ingest()
+
+    def test_local_ingest_should_return_file_data(self):
+        testing_file = "./tests/ingest_testing_sample.txt"
+        expected_data = ['this is\n', 'a simple\n', 'text file\n', 'for testing']
+        self.assertEqual(local_ingest(testing_file), expected_data)
