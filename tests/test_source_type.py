3a5e10cdb18faad97fe20341144ccbdeb0524309
Pranav <pranavbmcloud@gmail.com>
2021-01-02 12:13:49 +0530

    adds test_local_input_should_return_local_datasource

--- tests/test_source_type.py
+++ tests/test_source_type.py
@@ -9,8 +9,12 @@ class TestMainSourceType(unittest.TestCase):
     """Tests Source Type related functionality in the main.py module"""
     def setUp(self) -> None:
         self.input_url = "http://www.somedomain.com/path/input_file.json"
-
+        self.local_file = "file 01.json"
 
     def test_url_input_should_return_url_datasource(self):
         source_type = get_source_type(self.input_url)
         self.assertEqual(source_type, DataSource.URL)
+
+    def test_local_input_should_return_local_datasource(self):
+        source_type = get_source_type(self.local_file)
+        self.assertEqual(source_type, DataSource.LOCAL)
